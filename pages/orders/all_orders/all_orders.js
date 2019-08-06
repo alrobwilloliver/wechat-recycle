ac0821f690b5d401a8f4a5516d3c4f6bd856655a
Alan Oliver <alrobwilloliver@gmail.com>
2019-08-06 16:57:25 +0800

    Added new all_orders and my_orders files. Downloaded weui kit. Currently working on implementing navbar.

---
+++ pages/orders/all_orders/all_orders.js
@@ -0,0 +1,76 @@
+// pages/orders/all_orders/all_orders.js
+Page({
+  data: {
+    tabs: ["选项一", "选项二", "选项三"],
+    activeIndex: 1,
+    sliderOffset: 0,
+    sliderLeft: 0
+  },
+  onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  },
+
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
