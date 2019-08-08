9b7c98e3f6d76aa237f834a163fb421865d9ef7b
Alan Oliver <alrobwilloliver@gmail.com>
2019-08-08 23:43:05 +0800

    Added form on click feature. Displays address information in the order information

--- pages/customer/customer.js
+++ pages/customer/customer.js
@@ -53,8 +53,15 @@ Page({
     })
   },
   inputAddress: function (e) {
-    this.setData({
-    address: e.detail.value
+    // this.setData({
+    // address: e.detail.value
+    // })
+    wx.chooseLocation({
+      success: function (res) {
+        getApp().globalData.address = `${res.name}, ${res.address}`
+        getApp().globalData.coordinates = [res.longitude, res.latitude]
+        
+      }
     })
   },
   inputName: function (e) {
